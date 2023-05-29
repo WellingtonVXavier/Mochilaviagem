561e0e2815a319cb7765b94069cb7a3dfd19c488
wellington.xavier <sidious.lords@gmail.com>
2023-05-29 18:11:08 -0300

    mais itens
    atualizei o sistema

--- javascript/man.js
+++ javascript/man.js
@@ -6,15 +6,13 @@ form.addEventListener("submit", (evento) => {
 
     const nome = evento.target.elements['nome']
     const quantidade = evento.target.elements['quantidade']
-    
+
     criarElemento(nome.value, quantidade.value)
     nome.value = "";
     quantidade.value = "";
-
-
 })
 
-function criarElemento(nome, quantidade){
+function criarElemento(nome, quantidade) {
     const novoItem = document.createElement('li')
     novoItem.classList.add("item")
 
@@ -27,5 +25,11 @@ function criarElemento(nome, quantidade){
     lista.appendChild(novoItem)
     localStorage.setItem("nome", nome)
     localStorage.setItem("quantidade", quantidade)
-
 }
+
+let selecionaLista = [];
+let indice = 2; // índice do elemento que você deseja remover
+
+selecionaLista.splice(indice, 1); // Remove 1 elemento a partir do índice 2
+
+console.log(lista); 
